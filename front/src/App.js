78edb54984456963ffffe7387e2e85ca13bcac93
MAXFAD04 <fmax200704@yandex.ru>
2024-05-27 19:45:29 +0300

    fix meta

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <meta name="keywords" content="React, JavaScript, semantic markup, html" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src='/logo512.png' className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Автобазы. Статистика
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Начать
         </a>
       </header>
     </div>
