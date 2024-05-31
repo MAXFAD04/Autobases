2801b1d60197231a196408f6f5ef85d34b769247
Alexey Fadeev (Work) <fadeev@cnis.ru>
2024-05-31 13:53:44 +0300

    add filter by fuel

--- front/src/components/Automobiles/CarList.jsx
+++ front/src/components/Automobiles/CarList.jsx
@@ -7,6 +7,8 @@ const Automobiles = ({ baseid }) => {
 
   let [error, setError] = useState('');
   let [cars, setCars] = useState();
+  let [fuels, setFuels] = useState();
+  let [filter_fuel, setFilterFuel] = useState('');
   
   useEffect(() => {
     const getCars = async () => {
@@ -17,6 +19,12 @@ const Automobiles = ({ baseid }) => {
         if (data.error) setError(`<CarList> ${data.error}`);
         else if (data.length) {
           setCars(data);
+          const f = [];
+          for (const car of data) {
+            if (f.indexOf(car.fuel) === -1) f.push(car.fuel);
+          }
+          setFuels(f.sort());
+          setFilterFuel('');
         } else {
           setCars([]);
           setError('Автомобили не найдены');
@@ -28,6 +36,10 @@ const Automobiles = ({ baseid }) => {
     getCars()
   }, [ baseid ])
 
+  const filtered_cars = () => {
+    if (filter_fuel) return cars.filter(car=>car.fuel===filter_fuel)
+    return cars
+  }
 
   const handleAddCar = (newCar) => {
     setCars([...cars, newCar]);
@@ -42,10 +54,20 @@ const Automobiles = ({ baseid }) => {
   return (
       
     <section>
-      <h2>Список автомобилей</h2>
+      <h2 className='cars-header'>
+        <div>Список автомобилей</div>
+        <div className='fuel-filter'>
+          Фильтр:
+          <div>
+            {fuels && fuels.length && fuels.map((f) => (
+              <button onClick={() => setFilterFuel(f)}>{f}</button>
+            ))}
+          </div>
+        </div>
+      </h2>
       {error && (<div className='error'>Ошибка: {error}</div>)}
-      {cars && <div className='cars-list'>
-        {cars.map((car, index) => (
+      {filtered_cars() &&  <div className='cars-list'>
+        {filtered_cars().map((car, index) => (
           <CarCard            
             key={index}
             {...car}
